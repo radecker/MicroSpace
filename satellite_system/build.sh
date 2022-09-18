cb956df1a72110b75afea1a84519da451c8ba31a
lrpark <lrpark@live.com>
2022-09-18 15:19:02 -0400

    Fix hardcoded file paths in satellite_system/ build.sh scripts

--- satellite_system/build.sh
+++ satellite_system/build.sh
@@ -5,10 +5,10 @@ set -o pipefail
 # Run the common library build first
 cd ../common/
 ./build.sh
-cd ..
+cd ../satellite_system/hal_service/
 
 # Build the critical infrastructure (i.e. any service)
-cd ../hal_service/
+#cd ../hal_service/
 ./build.sh
 cd ../logger_service/
 ./build.sh
